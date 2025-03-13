07d4ce5c6733833be798370a327e036b320cde65
Grace <grace6424@icloud.com>
2025-03-13 11:24:25 +0100

    3.5.3.A3.dart

---
+++ 3.5.3.A3.dart
@@ -0,0 +1,31 @@
+import 'functions.dart';
+
+void main() {
+
+  clearConsole();
+
+  print('"Testing" contains ${aeiou('testing')} vowels');
+  print('"Another test" contains ${aeiou('another test')} vowels');
+  print('"Aaand another test contains ${aeiou('Aaand another test')} vowels');
+
+  lineBreak();
+
+}
+
+int aeiou(String text) {
+  int result = 0;
+  for (int i = 0; i < text.length; i++) {
+    if (text[i].contains('a')) {
+      result++;
+    } else if (text[i].contains('e')) {
+      result++;
+    } else if (text[i].contains('i')) {
+      result++;
+    } else if (text[i].contains('o')) {
+      result++;
+    } else if (text[i].contains('u')) {
+      result++;
+    }
+  }
+    return result;
+  }

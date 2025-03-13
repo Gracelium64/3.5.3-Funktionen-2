1d06ebf517becb932e896059da564fde76255b41
Grace <grace6424@icloud.com>
2025-03-13 10:55:02 +0100

    3.5.3.A1

---
+++ 3.5.3.A1.dart
@@ -0,0 +1,18 @@
+import 'functions.dart';
+
+void main() {
+
+  clearConsole();
+
+  print(sum(1, 2, 3));
+  print(sum(5, 5, 5));
+  print(sum(265, 3462, 3568386));
+
+  lineBreak();
+
+}
+
+int sum(int a, int b, int c) {
+  int result = a + b + c;
+  return result;
+}
---
+++ functions.dart
@@ -0,0 +1,7 @@
+void clearConsole() {
+  print("\x1B[2J\x1B[0;0H");
+}
+
+void lineBreak() {
+  print('');
+}

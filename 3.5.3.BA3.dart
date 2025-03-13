51b9d3faeb5db7c34ab87cfba58b74f474adc9c7
Grace <grace6424@icloud.com>
2025-03-13 13:14:34 +0100

    3.5.3.BA3

---
+++ 3.5.3.BA3.dart
@@ -0,0 +1,21 @@
+import 'functions.dart';
+
+void main() {
+
+  clearConsole();
+  bunny();
+
+  List<int> myList = [1, 2, 3, 4, 5];
+  print(sum(myList));
+
+  lineBreak();
+
+}
+
+int sum(List<int> a) {
+  int addedUp = 0;
+  for (int i = 0; i < a.length; i++) {
+    addedUp += a[i];
+  }
+  return addedUp;
+}
